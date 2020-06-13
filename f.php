24bb3740d2a36017df7ea39cb50a08fe8e259ba4
Mariem Hassan Hussien Ibrahiem <53480373+MariemHHussien@users.noreply.github.com>
2020-06-14 01:08:13 +0200

    Update f.php

--- f.php
+++ f.php
@@ -7,8 +7,8 @@ $phone=$_POST['phone'];
 $facebook=$_POST['facebook'];
 $Email=$_POST['Email'];}
 $AFTER=filter_var($age,FILTER_SANITIZE_NUMBER_INT);
-echo "Before Sanitize:".$age."</br>";
-echo "After Sanitize :".$AFTER."</br>";
+echo "Before Sanitize your age:".$age."</br>";
+echo "After Sanitize your age:".$AFTER."</br>";
 if(filter_var($AFTER,FILTER_VALIDATE_INT))
 {
     echo "DONE";
@@ -17,11 +17,11 @@ else
 {echo "we filter this";
 }
 echo "</br>";
-echo"*********************";
+echo"************************************************";
 echo "</br>";
 $a=filter_var($pass,FILTER_SANITIZE_NUMBER_INT);
-echo "Before Sanitize:".$pass."</br>";
-echo "After Sanitize :".$a."</br>";
+echo "Before Sanitize your pass:".$pass."</br>";
+echo "After Sanitize your pass".$a."</br>";
 if(filter_var($a,FILTER_VALIDATE_INT))
 {
     echo "DONE";
@@ -31,11 +31,11 @@ else
    echo "we filter this";
 }
 echo "</br>";
-echo"*********************";
+echo"************************************************";
 echo "</br>";
 $after=filter_var($phone,FILTER_SANITIZE_NUMBER_INT);
-echo "Before Sanitize:".$phone."</br>";
-echo "After Sanitize :".$after."</br>";
+echo "Before Sanitize your phone:".$phone."</br>";
+echo "After Sanitize your phone:".$after."</br>";
 if(filter_var($after,FILTER_VALIDATE_INT))
 {
     echo "DONE";
@@ -45,11 +45,11 @@ else
     echo "we filter this";
 }
 echo "</br>";
-echo"*********************";
+echo"************************************************";
 echo "</br>";
 $after=filter_var($facebook,FILTER_SANITIZE_URL);
-echo "Before Sanitize:".$facebook."</br>";
-echo "After Sanitize :".$after."</br>";
+echo "Before Sanitize your face:".$facebook."</br>";
+echo "After Sanitize your face:".$after."</br>";
 if(filter_var($after,FILTER_VALIDATE_URL))
 {
     echo "DONE";
@@ -59,11 +59,11 @@ else
     echo "This is not URL";
 }
 echo "</br>";
-echo"*********************";
+echo"************************************************";
 echo "</br>";
 $a=filter_var($Email,FILTER_SANITIZE_EMAIL);
-echo "Before Sanitize:".$Email."</br>";
-echo "After Sanitize :".$a."</br>";
+echo "Before Sanitize your Email:".$Email."</br>";
+echo "After Sanitize your Email:".$a."</br>";
 if(filter_var($a,FILTER_VALIDATE_EMAIL))
 {
     echo "DONE";
